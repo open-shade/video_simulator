ee993b7968c42a57ea905748ce3c0726b1f695ad
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-07-08 18:05:35 -0400

    Fix topic bug

--- camera_simulator/camera_simulator.py
+++ camera_simulator/camera_simulator.py
@@ -36,7 +36,7 @@ class CameraSimulator(Node):
     def __init__(self, **kwargs):
         super().__init__("camera_simulator")
 
-        image_topic_ = kwargs['output_topic']
+        image_topic_ = kwargs['topic']
         camera_info_topic_ = self.declare_parameter("camera_info_topic", "/image/camera_info").value
 
         self.frame_id_ = self.declare_parameter("frame_id", "camera").value
